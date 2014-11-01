b8db3c070ee63d7a0287bf305361cef0cfbc88b4
techsathish <ittechsathish@gmail.com>
2014-11-01 13:02:59 +0530

    js imple

---
+++ CareDeveloper.UI/CareDeveloper.Web/App/boot/app.js
@@ -0,0 +1,12 @@
+﻿define(['angularAMD', 'angular-route'], function (angularAMD) {
+    var app = angular.module("caredeveloper", ['ngRoute']);
+
+    app.config(function ($routeProvider) {
+        $routeProvider.when("/home", angularAMD.route({
+            templateUrl: 'views/home.html', controller: 'HomeCtrl',
+            controllerUrl: 'ctrl/home'
+        }))
+    });
+
+    return angularAMD.bootstrap(app);
+});
---
+++ CareDeveloper.UI/CareDeveloper.Web/App/boot/main.js
@@ -0,0 +1,11 @@
+﻿require.config({
+    baseUrl: "App",
+    paths: {
+        'angular': '.../Scripts/angular.min',
+        'angular-route': '.../Scripts/AngularUI/ui-router.min',
+        'angularAMD': '.../Scripts/angularAMD/angularAMD.min',
+        'app': 'boot/app'
+    },
+    shim: { 'angularAMD': ['angular'], 'angular-route': ['angular'] },
+    deps: ['app']
+});
