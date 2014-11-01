5ebc8da23fd91e0e5c5299ab5c3605a698792bcd
techsathish <ittechsathish@gmail.com>
2014-11-01 13:29:14 +0530

    co1

---
+++ CareDeveloper.UI/CareDeveloper.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace CareDeveloper.Web
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            //css 
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/css/cosmo/bootstrap.css",
+                "~/Content/font-awesome.css",
+                "~/Content/css/cosmo/site.css"
+            ));
+        }
+    }
+}
---
+++ CareDeveloper.UI/CareDeveloper.Web/App_Start/WebApiConfig.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace CareDeveloper.Web
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            //Site: CodeHelper START
+
+            // Controller Only
+            // To handle routes like `/api/codehelper/VTRouting`
+            config.Routes.MapHttpRoute(
+                name: "CodeHelperControllerOnly",
+                routeTemplate: "api/codehelper/{controller}"
+            );
+
+            // Controller with ID
+            // To handle routes like `/api/codehelper/VTRouting/1`
+            config.Routes.MapHttpRoute(
+                name: "CodeHelperControllerAndId",
+                routeTemplate: "api/codehelper/{controller}/{id}",
+                defaults: null,
+                constraints: new { id = @"^\d+$" } // Only integers 
+            );
+
+            // Controllers with Actions
+            // To handle routes like `/api/codehelper/VTRouting/route`
+            config.Routes.MapHttpRoute(
+                name: "CodeHelperControllerAndAction",
+                routeTemplate: "api/codehelper/{controller}/{action}"
+            );
+
+            //Site: CodeHelper ENDS --//
+
+            //Site: Main START
+
+            // Controller Only
+            // To handle routes like `/api/VTRouting`
+            config.Routes.MapHttpRoute(
+                name: "ControllerOnly",
+                routeTemplate: "api/{controller}"
+            );
+
+            // Controller with ID
+            // To handle routes like `/api/VTRouting/1`
+            config.Routes.MapHttpRoute(
+                name: "ControllerAndId",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: null,
+                constraints: new { id = @"^\d+$" } // Only integers 
+            );
+
+            // Controllers with Actions
+            // To handle routes like `/api/VTRouting/route`
+            config.Routes.MapHttpRoute(
+                name: "ControllerAndAction",
+                routeTemplate: "api/{controller}/{action}"
+            );
+
+            //Site: Main ENDS --//
+        }
+    }
+}
