aec795f261835cff6088fd9cccd93b2a39c117bc
techsathish <ittechsathish@gmail.com>
2014-11-01 14:15:52 -0700

    1

--- CareDeveloper.UI/CareDeveloper.Web/App/boot/app.js
+++ CareDeveloper.UI/CareDeveloper.Web/App/boot/app.js
@@ -1,12 +1,20 @@
 ﻿define(['angularAMD', 'angular-route'], function (angularAMD) {
-    var app = angular.module("caredeveloper", ['ngRoute']);
+    var app = angular.module("caredeveloper", ['ui-router']);
 
-    app.config(function ($routeProvider) {
-        $routeProvider.when("/home", angularAMD.route({
-            templateUrl: 'views/home.html', controller: 'HomeCtrl',
-            controllerUrl: 'ctrl/home'
-        }))
-    });
+    app.config(
+            ['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+                $urlRouterProvider.otherwise("/page-not-found");
+
+                $stateProvider
+                .state('home', angularAMD.route({
+                    url: '/',
+                    controller: 'caredeveloper.home',
+                    controllerUrl: '/App/home/controller/home.js',
+                    templateUrl: '/App/home/view/home.html',
+                }))
+                ;
+            }]
+        );
 
     return angularAMD.bootstrap(app);
 });
